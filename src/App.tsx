9469db10a40627798bf6263fff01ca75812731eb
Tony-Jack2017 <815378559@qq.com>
2022-08-22 22:24:58 +0800

    fix: eslint style

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
-import React from 'react';
-import './App.css';
-import {Button} from "antd";
+import React from 'react'
+import './App.css'
+import { Button } from 'antd'
 
-function App() {
-    return (
-        <div className="App">
-            <Button type="link">Hello World</Button>
-        </div>
-    );
-}
+// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+const App = () => (
+    <div className="App">
+        <Button type="link">Hello World</Button>
+    </div>
+);
 
-export default App;
+export default App
